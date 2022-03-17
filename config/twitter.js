acad36e0490699ebf335f55fabb30be5222ecd08
Ike Oche <ikeemma.emmanuel@gmail.com>
2022-03-17 09:31:01 +0100

    processed env variables

--- config/twitter.js
+++ config/twitter.js
@@ -1,3 +1,4 @@
+// process environment variables and expose for use
 export const configTwitter = {
         consumer_key: process.env.CONSUMER_KEY,
     	consumer_secret: process.env.CONSUMER_SECRET,
